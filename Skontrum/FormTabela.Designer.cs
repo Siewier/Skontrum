761b4b491582b6218f2d52c46fe52ca95c1111e7
Siewier <przemeksiewierski@gmail.com>
2014-12-07 15:06:57 +0100

    Poprawki do tabeli

--- Skontrum/FormTabela.Designer.cs
+++ Skontrum/FormTabela.Designer.cs
@@ -40,7 +40,6 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(13, 13);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(368, 336);
             this.dataGridView1.TabIndex = 0;
             // 
